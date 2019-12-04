d5981a3d4fbbcd07c533f446a190cefc76526746
Lubomir Kastovsky <lubomir.kastovsky@avast.com>
2019-12-04 23:10:45 +0100

    solved 01, added 02

--- AOC2019_01/AOC2019_01/main.swift
+++ AOC2019_01/AOC2019_01/main.swift
@@ -8,4 +8,34 @@
 
 import Foundation
 
+let input = readLinesRemoveEmpty(str: inputString)
+let modules = strArrayToIntArray(strArray: input)
 
+func fuelForMassSum(modules: Array<Int>) -> Int {
+	return modules.reduce(0, { result, mass in
+		result + ((mass / 3) - 2)
+	})
+}
+
+print("1. " + String(fuelForMassSum(modules: modules)))
+
+func oneModuleFuel(mass: Int) -> Int {
+	var remainigMass = mass
+	var fuelSum = 0
+	while remainigMass > 0 {
+		let fuelMass = (remainigMass / 3) - 2
+		if fuelMass > 0 {
+			fuelSum += fuelMass
+		}
+		remainigMass = fuelMass
+	}
+	return fuelSum
+}
+
+func fuelForMassSumIncludingFuelMass(modules: Array<Int>) -> Int {
+	return modules.reduce(0, { result, mass in
+		result + oneModuleFuel(mass: mass)
+	})
+}
+
+print("2. " + String(fuelForMassSumIncludingFuelMass(modules: modules)))
---
+++ AOC2019_02/AOC2019_02/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  AOC2019_02
+//
+//  Created by Lubomir Kastovsky on 04/12/2019.
+//  Copyright © 2019 Lubomir Kastovsky. All rights reserved.
+//
+
+import Foundation
+
+
