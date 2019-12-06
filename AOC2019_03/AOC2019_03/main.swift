89bb55a334763985d2acca070d82839ef817d390
Lubomir Kastovsky <lubomir.kastovsky@avast.com>
2019-12-06 23:43:01 +0100

    03 part 1

--- AOC2019_03/AOC2019_03/main.swift
+++ AOC2019_03/AOC2019_03/main.swift
@@ -62,142 +62,79 @@ func createLines(wirePath: [String]) -> [Line] {
 	var end = Point(x: 0, y: 0)
 	wirePath.forEach { step in
 		let len = length(step: step)
-		var line: Line = Line(
-		switch direction(step: step) {
-		case .Up: end = Point(x: start.x, y: start.y + len)
-
-		case .Down: end = Point(x: start.x, y: start.y - len)
-		case .Right: end = Point(x: start.x + len, y: start.y)
-		case .Left: end = Point(x: start.x - len, y: start.y)
-		default: break
-		}
-		lines.append(Line(start: start, end: end))
+        var line = Line(points: [])
+		let dir = direction(step: step)
+        if dir == .Up || dir == .Down {
+            if dir == .Up {
+                end = Point(x: start.x, y: start.y + len)
+            }
+            if dir == .Down {
+                end = Point(x: start.x, y: start.y - len)
+            }
+            if start.y < end.y {
+                for y in start.y...end.y {
+                    line.points.insert(Point(x: start.x, y: y))
+                }
+            } else {
+                for y in end.y...start.y {
+                    line.points.insert(Point(x: start.x, y: y))
+                }
+            }
+        }
+        if dir == .Right || dir == .Left {
+            if dir == .Right {
+                end = Point(x: start.x + len, y: start.y)
+            }
+            if dir == .Left {
+                end = Point(x: start.x - len, y: start.y)
+            }
+            if start.x < end.x {
+                for x in start.x...end.x {
+                    line.points.insert(Point(x: x, y: start.y))
+                }
+            } else {
+                for x in end.x...start.x {
+                    line.points.insert(Point(x: x, y: start.y))
+                }
+            }
+        }
 		start = end
+        lines.append(line)
 	}
 	return lines
 }
 
-func hasIntersectionX(firstLine: Line, secondLine: Line) -> Bool {
-	let ax1 = firstLine.start.x
-	let ax2 = firstLine.end.x
-	let bx1 = secondLine.start.x
-	let bx2 = secondLine.end.x
-	if (ax1 <= ax2) && ((ax1 <= bx1 && bx1 <= ax2) || (ax1 <= bx2 && bx2 <= ax2)) {
-		return true
-	}
-	if (ax2 < ax1) && ((ax2 <= bx1 && bx1 <= ax1) || (ax2 <= bx2 && bx2 <= ax1)) {
-		return true
-	}
-	return false
+func hasIntersection(line1: Line, line2: Line) -> Bool {
+    return !line1.points.intersection(line2.points).isEmpty
 }
 
-func hasIntersectionY(firstLine: Line, secondLine: Line) -> Bool {
-	let ay1 = firstLine.start.y
-	let ay2 = firstLine.end.y
-	let by1 = secondLine.start.y
-	let by2 = secondLine.end.y
-	if (ay1 <= ay2) && ((ay1 <= by1 && by1 <= ay2) || (ay1 <= by2 && by2 <= ay2)) {
-		return true
-	}
-	if (ay2 < ay1) && ((ay2 <= by1 && by1 <= ay1) || (ay2 <= by2 && by2 <= ay1)) {
-		return true
-	}
-	return false
-}
-
-func isSameLine(line1: Line, line2: Line) -> Bool {
-	return line1.start.x == line2.start.x &&
-		line1.end.x == line2.end.x &&
-		line1.start.y == line2.start.y &&
-		line1.end.y == line2.end.y
-}
-
-func linesIntersection(firstLine: Line, secondLine: Line) -> Point? {
-	if firstLine.start.x == firstLine.end.x {
-		// vertical line
-		var starty = firstLine.start.y
-		var endy = firstLine.end.y
-		if starty > endy {
-			starty = firstLine.end.y
-			endy = firstLine.start.y
-		}
-		for y in starty...endy {
-			if secondLine.start.x == secondLine.end.x {
-				// vertical line, dont know if can overlap???
-				// probably not
-				return nil
-			} else {
-				var startx = secondLine.start.x
-				var endx = secondLine.end.x
-				if startx > endx {
-					startx = secondLine.end.x
-					endx = secondLine.start.x
-				}
-				for x in startx...endx {
-					if firstLine.start.x == x && y == secondLine.start.y {
-						return Point(x: x, y: y)
-					}
-				}
-			}
-		}
-	} else {
-		// horizontal line
-		var startx = firstLine.start.x
-		var endx = firstLine.end.x
-		if startx > endx {
-			startx = firstLine.end.x
-			endx = firstLine.start.x
-		}
-		for x in startx...endx {
-			if secondLine.start.y == secondLine.end.y {
-				// horizontal line, no intersection
-				return nil
-			} else {
-				var starty = secondLine.start.y
-				var endy = secondLine.end.y
-				if starty > endy {
-					starty = secondLine.end.y
-					endy = secondLine.start.y
-				}
-				for y in starty...endy {
-					if x == secondLine.start.x && firstLine.start.y == y {
-						return Point(x: x, y: y)
-					}
-				}
-			}
-		}
-	}
-	return nil
+func intersectingPoint(line1: Line, line2: Line) -> Point? {
+    return line1.points.intersection(line2.points).first
 }
 
 let wire1Lines = createLines(wirePath: wirePath1)
 let wire2Lines = createLines(wirePath: wirePath2)
 
-print(wire1Lines)
-print(wire2Lines)
+//print(wire1Lines)
+//print(wire2Lines)
 
 func closestIntersectionDistance(wireLines1: Array<Line>, wireLines2: Array<Line>) -> Int {
 	var distance = Int.max
-	wireLines1.forEach { line1 in
-		wireLines2.forEach { line2 in
-			print("line1: ", line1)
-			print("line2: ", line2)
-			print("")
-			if !isSameLine(line1: line1, line2: line2) {
-				if hasIntersectionX(firstLine: line1, secondLine: line2) &&
-					hasIntersectionY(firstLine: line1, secondLine: line2) {
-					if let intersectionPoint = linesIntersection(firstLine: line1, secondLine: line2) {
-						print(intersectionPoint)
-						if !(intersectionPoint.x == 0 && intersectionPoint.y == 0) {
-							let dist = abs(intersectionPoint.x) + abs(intersectionPoint.y)
-							if dist < distance {
-								distance = dist
-								print(distance)
-							}
-						}
-					}
-				}
-			}
+    for i in 0..<wireLines1.count {
+        let line1 = wireLines1[i]
+        for j in 0..<wireLines2.count {
+            let line2 = wireLines2[j]
+            if hasIntersection(line1: line1, line2: line2) {
+                if let intersection = intersectingPoint(line1: line1, line2: line2) {
+                    if !(intersection.x == 0 && intersection.y == 0) {
+                        let dist = abs(intersection.x) + abs(intersection.y)
+                        if dist < distance {
+                            distance = dist
+                            print(distance)
+                        }
+                    }
+                }
+            }
 		}
 	}
 	return distance
